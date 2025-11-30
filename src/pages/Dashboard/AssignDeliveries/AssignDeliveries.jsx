f99e1854d94d936aa95509bf51f810bb1059e958
Programmer Rakibul <rakibul00206@gmail.com>
2025-11-30 12:21:37 +0600

    parcel accept and reject functionality for a rider completed

--- src/pages/Dashboard/AssignDeliveries/AssignDeliveries.jsx
+++ src/pages/Dashboard/AssignDeliveries/AssignDeliveries.jsx
@@ -1,12 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import useAuthInfo from "../../../hooks/useAuthInfo";
 import useSecureAxios from "../../../hooks/useSecureAxios";
+import Swal from "sweetalert2";
 
 const AssignDeliveries = () => {
   const { currentUser } = useAuthInfo();
   const secureAxios = useSecureAxios();
 
-  const { data: parcels = [], isPending } = useQuery({
+  const {
+    data: parcels = [],
+    isPending,
+    refetch,
+  } = useQuery({
     queryKey: ["parcels", "rider_assigned", currentUser.email],
     queryFn: async () => {
       const { data } = await secureAxios.get(
@@ -21,49 +26,98 @@ const AssignDeliveries = () => {
     return <p>Loading...</p>;
   }
 
-  console.log(parcels);
+  const handleDeliveryStatus = async (
+    parcel_id,
+    delivery_status,
+    work_status
+  ) => {
+    const deliveryInfo = {
+      rider_email: currentUser.email,
+      delivery_status,
+      work_status,
+    };
+
+    try {
+      const { data } = await secureAxios.patch(
+        `/parcels/${parcel_id}/rider`,
+        deliveryInfo
+      );
+
+      if (data.modifiedCount) {
+        refetch();
+
+        Swal.fire({
+          icon: "success",
+          title: `Parcel ${work_status ? "rejected" : "accepted"}`,
+          timer: 2000,
+        });
+      }
+
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
-    <section>
-      <div>
-        <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
-          <table className="table table-zebra">
-            {/* head */}
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Parcel Name</th>
-                <th>Pickup Address</th>
-                <th>Delivery Address</th>
-                <th>Amount</th>
-                <th>Action</th>
-              </tr>
-            </thead>
+    <>
+      <title>Assign Deliveries</title>
 
-            <tbody>
-              {parcels.map((parcel, index) => (
-                <tr key={parcel._id}>
-                  <th>{index + 1}</th>
-                  <td>{parcel.parcel_name}</td>
-                  <td className="capitalize">{parcel.sender_address}</td>
-                  <td className="capitalize">{parcel.receiver_address}</td>
-                  <td className="capitalize">${parcel.deliveryCharge}</td>
-                  <td className="flex items-center gap-1.5">
-                    <button className="btn btn-sm btn-primary text-neutral">
-                      Accept
-                    </button>
-
-                    <button className="btn btn-sm btn-warning text-neutral">
-                      Reject
-                    </button>
-                  </td>
+      <section>
+        <div>
+          <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+            <table className="table table-zebra">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Parcel Name</th>
+                  <th>Pickup Address</th>
+                  <th>Delivery Address</th>
+                  <th>Amount</th>
+                  <th>Action</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+
+              <tbody>
+                {parcels.map((parcel, index) => (
+                  <tr key={parcel._id}>
+                    <th>{index + 1}</th>
+                    <td>{parcel.parcel_name}</td>
+                    <td className="capitalize">{parcel.sender_address}</td>
+                    <td className="capitalize">{parcel.receiver_address}</td>
+                    <td className="capitalize">${parcel.deliveryCharge}</td>
+                    <td className="flex items-center gap-1.5">
+                      <button
+                        onClick={() =>
+                          handleDeliveryStatus(parcel._id, "rider_arriving")
+                        }
+                        className="btn btn-sm btn-primary text-neutral"
+                      >
+                        Accept
+                      </button>
+
+                      <button
+                        onClick={() =>
+                          handleDeliveryStatus(
+                            parcel._id,
+                            "pending_pickup",
+                            "available"
+                          )
+                        }
+                        className="btn btn-sm btn-warning text-neutral"
+                      >
+                        Reject
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
